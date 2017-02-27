49607ac3f4a2556222cb2b884ed3aca38919dacc
Cédric Frêcheville <frecheville.cedric@gmail.com>
2017-02-27 16:32:45 +0100

    scoping

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,15 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+
+    this.data = [];
+
+    this.add = () => {
+    	this.user = {
+    		"firstname" : this.firstname,
+    		"lastname" : this.lastname,
+    		"address" :  this.address,
+    	};
+
+    	this.data.push(this.user);
+    };
 }
